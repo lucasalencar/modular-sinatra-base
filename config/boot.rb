e6aae9b803e19b8907c43d8bc21d83f7c0483a15
Lucas André de Alencar <alencar.lucas.a@gmail.com>
2012-09-20 15:51:34 -0300

    Corrected database declaration.

--- config/boot.rb
+++ config/boot.rb
@@ -8,8 +8,4 @@ require File.expand_path("../environment", __FILE__)
 
 Dir[File.expand_path("../../models/**/*", __FILE__)].each { |model| require model }
 
-# Loads configurations of the database and establish a connection
-require "yaml"
-db_file_path = File.expand_path('../database.yml', __FILE__)
-dbconfig = YAML::load(File.open(db_file_path))
-ActiveRecord::Base.establish_connection(dbconfig)
+set :database, "sqlite:///#{Sinatra::Base.environment}.db"
